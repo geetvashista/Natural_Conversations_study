86a07385f2956a4997604e5e8f18940e6dd1d986
Geet Vashista <148626773+geetvashista@users.noreply.github.com>
2024-11-10 13:28:23 +1300

    Create Google_get_and_save_data_v2.py

---
+++ Google_get_and_save_data_v2.py
@@ -0,0 +1,17 @@
+
+
+master_dir = '/media/sahib/macos/root/LCMV_output_19092024'
+save_dir = '/media/sahib/Mangor_2TB/Geet/Tasks/interviewer_repetition/Gamma/' # don't forget the / on the end!
+
+def get_and_save_data(master_dir, band, task, save_dir):
+    sub = 0
+    for filename in os.listdir(master_dir):
+        if '-epo' in filename:
+            if task in filename:
+                if band in filename:
+                    data_as_epo = mne.read_epochs(os.path.join(master_dir, filename))
+                    data = data_as_epo.get_data()
+                    np.save(save_dir + task + '_' + band + '_sub_' + str(sub), data)
+                    sub = sub + 1
+
+get_and_save_data(master_dir=master_dir, band=band[3], task=tasks[1], save_dir=save_dir)
